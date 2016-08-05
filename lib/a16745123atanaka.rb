6db151e720d0b49e45a37ca0497198717ad55e1c
a-rom <a1623at@aiit.ac.jp>
2016-08-05 14:22:23 +0000

    add fizzbuzz

--- lib/a16745123atanaka.rb
+++ lib/a16745123atanaka.rb
@@ -1,7 +1,4 @@
-require "a16745123atanaka/version"
-
-module A16745123atanaka
-  def fb(n)
+def fb(n)
     if n%3==0 and n%5==0
 		ret= "FizzBuzz"
 	elsif n%3==0
@@ -20,4 +17,4 @@ while i<=10000000
 	puts i,fb(i)
 	i= i+1
 end
-end
+
