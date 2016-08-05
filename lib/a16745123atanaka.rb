0645c507f7d590d6beef41839c3bcd778c082627
a-rom <a1623at@aiit.ac.jp>
2016-08-05 13:51:14 +0000

    first commit

--- lib/a16745123atanaka.rb
+++ lib/a16745123atanaka.rb
@@ -1,5 +1,23 @@
 require "a16745123atanaka/version"
 
 module A16745123atanaka
-  puts 'Hello World'
+  def fb(n)
+    if n%3==0 and n%5==0
+		ret= "FizzBuzz"
+	elsif n%3==0
+		ret= "Fizz"
+	elsif n%5==0
+		ret= "Buzz"
+	else
+		ret= ""
+
+	return ret
+	end
+end
+
+i= 1
+while i<=10000000
+	puts i,fb(i)
+	i= i+1
+end
 end
